eac9d6ca5046b93a9be1b4327b8593729a7330a0
Thun An <thun.chiu@pitang.com>
2018-01-26 13:08:28 -0200

    Criada a Pasta de Entidades

---
+++ EstudoProjeto/Projeto.Entites/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto.Entites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
